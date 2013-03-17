101a039da057a65088aada169d293010fed281cd
tushar_babbar_inox <tushar.babbar@inoxapps.com>
2013-03-17 13:42:10 +0530

    cron_changed

--- cron/cron.rb
+++ cron/cron.rb
@@ -2,9 +2,6 @@ x = File.dirname(File.expand_path('__FILE__'))
 require x+'/dbupdater.rb'
 	
 dbupdater = DbUpdater.new
-<<<<<<< HEAD
-dbupdater.updateDb
-=======
 dbupdater.update_Db
->>>>>>> work
+
 
